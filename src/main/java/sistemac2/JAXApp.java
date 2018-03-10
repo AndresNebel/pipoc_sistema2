0557bcadf5c288f0938036ed3d665f2dd794a542
Andres <andres@localhost.localdomain>
2018-03-10 18:47:23 -0300

    Primer commit. Agrego sistema 2

---
+++ src/main/java/sistemac2/JAXApp.java
@@ -0,0 +1,9 @@
+package sistemac2;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("pipoc")
+public class JAXApp extends Application {
+
+}
---
+++ src/main/java/sistemac2/sistemaC2.java
@@ -0,0 +1,45 @@
+package sistemac2;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("getOrders")
+public class sistemaC2 {
+	final static String orderTemplate = "\n" + 
+			"<order>\n" + 
+			"  <ID>__orderID__</ID>\n" + 
+			"  <name>Orden de importación</name>\n" + 
+			"  <data>__timestamp__</date>\n" +
+			"  <value>__value__</value>\n" +
+			"</order>";
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public String getOrders() {
+		Date now = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(now);
+		Random randomGenerator = new Random(now.getTime());
+		
+		String orders = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
+						"<orders>";
+
+		int randomLowNumber = calendar.get(Calendar.SECOND)*2; //un numero entre 0 y 120
+		for (int i=0; i<randomLowNumber; i++) {
+			String idGenerated = Integer.toString(randomGenerator.nextInt());
+			String value = Integer.toString(randomGenerator.nextInt());
+			String newOrder = orderTemplate.replace("__orderID__", idGenerated)
+								.replace("__timestamp__", now.toString())
+								.replace("__value__", value); //I am the senate!
+			orders += "\n"+newOrder; 
+		}
+		return orders;		
+	}
+	
+}
