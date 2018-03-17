0bdc30ca6025c87c3bac4ea4fb6d5d0b9c40b591
Andres <andres@localhost.localdomain>
2018-03-17 17:03:15 -0300

    metrics

--- src/main/java/sistemac2/sistemaC2.java
+++ src/main/java/sistemac2/sistemaC2.java
@@ -1,5 +1,6 @@
 package sistemac2;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
@@ -17,25 +18,30 @@ public class sistemaC2 {
 			"  <name>Orden de importación</name>\n" + 
 			"  <date>__timestamp__</date>\n" +
 			"  <value>__value__</value>\n" +
+			"  <orderset>__orderset__</orderset>\n" +
 			"</order>";
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public String getOrders() {
 		Date now = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		String dateString = dateFormat.format(now);
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(now);
 		Random randomGenerator = new Random(now.getTime());
 		
 		String orders = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
 						"<orders>";
-
-		int randomLowNumber = calendar.get(Calendar.SECOND)*2; //un numero entre 0 y 120
+		
+		
+		int randomLowNumber = calendar.get(Calendar.SECOND)*5; //un numero entre 0 y 300
 		for (int i=0; i<randomLowNumber; i++) {
 			String idGenerated = Integer.toString(randomGenerator.nextInt());
 			String value = Integer.toString(randomGenerator.nextInt());
 			String newOrder = orderTemplate.replace("__orderID__", idGenerated)
-								.replace("__timestamp__", now.toString())
+								.replace("__timestamp__", dateString)
+								.replace("__orderset__", Integer.toString(randomLowNumber))
 								.replace("__value__", value); //I am the senate!
 			orders += "\n"+newOrder; 
 		}
